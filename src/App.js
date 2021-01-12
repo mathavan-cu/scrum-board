ef83b0acd5dc99d568d7cbf6e1866380070ea797
sank2000 <santhoshvelr@yahoo.com>
2021-01-12 13:47:38 +0530

    scrum board added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import './styles/styles.scss';
+import { useState, useEffect } from 'react';
+import Board from 'react-trello';
+import classes from './style.module.scss';
+
+const initial = {
+  lanes: [
+    {
+      id: 'lane1',
+      title: 'To do',
+      cards: [
+        {
+          id: 'Card1',
+          title: 'Write Blog',
+          description: 'Can AI make memes',
+          label: '30 mins',
+        },
+        {
+          id: 'Card2',
+          title: 'Pay Rent',
+          description: 'Transfer via NEFT',
+          label: '5 mins',
+          metadata: { sha: 'be312a1' },
+        },
+      ],
+    },
+    {
+      id: 'lane2',
+      title: 'Completed',
+      cards: [],
+    },
+  ],
+};
 
 function App() {
+  const [data, setData] = useState(
+    localStorage.getItem('scrum')
+      ? JSON.parse(localStorage.getItem('scrum'))
+      : initial
+  );
+
+  useEffect(() => {
+    if (
+      data &&
+      JSON.stringify(data) !== JSON.parse(localStorage.getItem('scrum'))
+    ) {
+      localStorage.setItem('scrum', JSON.stringify(data));
+    }
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="App">
+      <header className={classes.header}>
+        <h1>Scrum Board</h1>
       </header>
-    </div>
+      <Board
+        data={data}
+        draggable
+        editable
+        className={classes.main}
+        onDataChange={(newData) => setData({ ...newData })}
+        canAddLanes
+        laneStyle={{ margin: '0 2rem' }}
+      />
+    </section>
   );
 }
 
